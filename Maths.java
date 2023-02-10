982acf1e5f5b18bf296f17f6a2243c357d4b3a56
Wanderson <wanderson.wf@pg.com>
2023-02-10 10:42:28 -0400

    Maths (x) commented output

---
+++ Maths.java
@@ -0,0 +1,25 @@
+public class Maths {
+    public static void main(String[] args) {
+        System.out.println("---------------Maths---------------");
+
+        // Find the highest value
+        System.out.println(Math.max(5, 10)); // 10
+        
+        // Find the lowest value
+        System.out.println(Math.min(5, 10)); // 5
+
+        // Square root
+        System.out.println(Math.sqrt(64)); // 8.0
+
+        // Absolute 
+        System.out.println(Math.abs(-4.7)); // 4.7
+
+        // Random number between 0.0(in) and 1.0(out)
+        System.out.println(Math.random()); // any like 0.6840760914902435
+
+        // Random number between 0 and 100
+        System.out.println((int)(Math.random() * 101)); // any like 79
+
+
+    }
+}
